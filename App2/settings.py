990a488749765ff9e86c2f8574686a681b04f8a8
ShamanS1 <shamanhussain@gmail.com>
2023-08-11 17:18:32 +0530

    commit

--- App2/settings.py
+++ App2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-stup&b)l$q$&1==odob!fja#k__4(^zimeod4b=xj9bh^-e!4a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["django-app2-bs5x.onrender.com","127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
