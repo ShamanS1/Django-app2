76c324ca75ca00e5a78e98e36e4c38f443885296
ShamanS1 <shamanhussain@gmail.com>
2023-08-11 17:43:24 +0530

    commit

--- Task/urls.py
+++ Task/urls.py
@@ -7,6 +7,7 @@ from django.views.static import serve
 
 re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+re_path(r'^static/(?P<path>.*)$', serve,{'document_file': settings.STATICFILES_DIRS}),
 urlpatterns = [
     path('', views.index, name='index'),
     
