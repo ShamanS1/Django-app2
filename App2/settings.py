d8fdf22ac4cf149a08746dd910ed63b695430284
ShamanS1 <shamanhussain@gmail.com>
2023-08-11 19:02:17 +0530

    commit

--- App2/settings.py
+++ App2/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
